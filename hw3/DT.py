762ec49d9a079384b266543648cb1e3e566d4ef6
liorsher <lior.sherman@intel.com>
2020-06-20 21:54:31 +0300

    Newest

--- hw3/DT.py
+++ hw3/DT.py
@@ -37,7 +37,7 @@ def majorityClass(data: pd.DataFrame)->int:
 def get_class_from_example(example: pd.DataFrame) -> int:
     return example['diagnosis']
 
-def is_all_examples_have_same_class(classification:int ,examples: pandas.DataFrame) -> bool:
+def allExamplesAreSameClass(classification, examples_data: pd.DataFrame) -> bool:
     # classification = get_class_from_example(examples.iloc[[0]])
     for example in examples_data.iterrows():
         if classification != example[1]['diagnosis'] :
@@ -45,38 +45,12 @@ def is_all_examples_have_same_class(classification:int ,examples: pandas.DataFra
     return True
 
 
-
-def ID3(examples:pandas.DataFrame,features:list):
-    best_feature, best_treshold, bestIG = None, 0, 0
-    classification = majorityClass(examples)
-    return TDIDT(examples,features,classification,MaxIG)
-def MaxIG(features:list,Examples:pandas.DataFrame):
-    def get_counts(df:pd.DataFrame) -> (int, int , int):
-        zero_count = len(df.loc[df['diagnosis'] == 0])
-        one_count = len(df.loc[df['diagnosis'] == 1])
-        return zero_count,one_count,(zero_count+one_count)
-
-    for feature in features;
-        fraction = (float(numerator)/float(denominator))
-        if fraction == 0:
-            return 0
-        return (fraction)*math.log(fraction,2)
-        #print(str(feature))
-    left_count_zero, left_count_one,left_count_total =get_counts(left_examples)
-    right_count_zero, right_count_one, right_count_total = get_counts(right_examples)
-    father_count_zero, father_count_one, father_count_total = get_counts(examples)
-    left_h =- (calculate(left_count_zero,left_count_total)+calculate(left_count_one,left_count_total))
-    right_h = - (calculate(right_count_zero, right_count_total) + calculate(right_count_one, right_count_total))
-    father_h = - (calculate(father_count_zero, father_count_total) + calculate(father_count_one, father_count_total))
-        examples_sorted_by_feature =examples_data.sort_values(by=[feature])
-    return  father_h -((left_count_total/father_count_total)*left_h)- ((right_count_total/father_count_total)*right_h)
-
-
-
 def MaxIG(examples_data:pd.DataFrame):
     best_feature, best_treshold, bestIG = None, 0, 0
     features = [f for f in examples_data.columns[1:]]
+    #print(len(examples_data))
     for feature in features:
+        #print(str(feature))
         examples_sorted_by_feature =examples_data.sort_values(by=[feature])
         feature_values_list=list(examples_sorted_by_feature[feature])
         tresholds = [(x+y)/2 for x,y in zip(feature_values_list, feature_values_list[1:])]
@@ -93,7 +67,7 @@ def basicLeafTest(examples_data:pd.DataFrame):
         return Node(None, None, [], DEFAULT_CLASSIFICATION)
 
     classification = majorityClass(examples_data)
-def TDIDT(examples:pandas.DataFrame, features:list,classification, select_feature_function):
+    if allExamplesAreSameClass(classification=classification, examples_data=examples_data):
         return Node(None, None, [], classification)
 
     #not a leaf
@@ -105,15 +79,16 @@ class LessThenXExamples:
         self.x =x
     def checkOn(self,examples_data:pd.DataFrame):
         if len(examples_data) <= self.x:
-    if examples.empty:
-        return Junction(None, [], DEFULT)
+            return false
+        return true
 
 def ID3(examples_data:pd.DataFrame,selectFeature_f = MaxIG, ifLeafMakeLeaf_f = basicLeafTest):
-    classification = majorityClass(examples_data)
+    #classification = majorityClass(examples_data)
     return TDIDT(examples_data, selectFeature_f, ifLeafMakeLeaf_f)
+
 def calcIG(examples,left_examples:pd.DataFrame,right_examples:pd.DataFrame):
-    if len(list(examples.columns)) == 1 or is_all_examples_have_same_class(classification=classification,
-                                                                           examples= examples):
+
+    def get_counts(df:pd.DataFrame) -> (int, int , int):
         zero_count = len(df.loc[df['diagnosis'] == 0])
         one_count = len(df.loc[df['diagnosis'] == 1])
         return zero_count,one_count,(zero_count+one_count)
@@ -122,16 +97,16 @@ def calcIG(examples,left_examples:pd.DataFrame,right_examples:pd.DataFrame):
         if float(numerator) == 0:
             return 0
         fraction = (float(numerator)/float(denominator))
-        return Junction(None,[],classification)
+        return (fraction)*math.log(fraction,2)
+
     left_count_zero, left_count_one,left_count_total =get_counts(left_examples)
     right_count_zero, right_count_one, right_count_total = get_counts(right_examples)
-    feature = select_feature_function(features, examples)
-    right_child = TDIDT(examples_data[examples_data[feature] > treshold], selectFeatureFunction)
+    father_count_zero, father_count_one, father_count_total = get_counts(examples)
+    left_h =- (calculate(left_count_zero,left_count_total)+calculate(left_count_one,left_count_total))
+    right_h = - (calculate(right_count_zero, right_count_total) + calculate(right_count_one, right_count_total))
     father_h = - (calculate(father_count_zero, father_count_total) + calculate(father_count_one, father_count_total))
 
-    if len(tree.children)==0:
-        return tree.classification
-    return DT_Classify(obj, tree.getNextChild(obj))
+    return  father_h -((left_count_total/father_count_total)*left_h)- ((right_count_total/father_count_total)*right_h)
 
 
 
@@ -176,6 +151,7 @@ if __name__ == '__main__':
 
     if RECALC ==1:
         start_time = time.time()
+        classifier = TDIDT(train_data, MaxIG)
         time_ = time.time() -start_time
         print(str(time))
         to_file = open("ClassifierBuild_1", "wb")
@@ -188,6 +164,8 @@ if __name__ == '__main__':
 
     ok =0
     not_ok =0
+    for example in test_data.iterrows():
+        if DT_Classify(example,classifier)==example[1]['diagnosis']:
             ok+=1
         else:
             not_ok += 1
